1934bc98b52f9687c30bbb155fe1a47efb02cf84
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-09-12 20:07:27 +0200

    Initial commit with pwa fix

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Elektroniczy System Zgłaszania Usterek Technicznych',
+      short_name: 'ESZUT',
+      description: 'Aplikacja kliencka do systemu ESZUT, umozliwia zgłaszanie usterek związanych z niesprawnym sprzętem.',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
