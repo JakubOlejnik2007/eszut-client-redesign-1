8d4645e3099f6c2992a34374ada3e30637ceaa60
Mati <m.stoch2007@gmail.com>
2024-09-12 21:32:22 +0200

    removed default css, i'd win

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,14 @@ import appLogo from '/favicon.svg'
 import PWABadge from './PWABadge.tsx'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="Elektroniczy System Zgłaszania Usterek Technicznych logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <nav className='navbar'>  <div className='logo'>ESZUT</div> </nav>  
+
       <h1>Elektroniczy System Zgłaszania Usterek Technicznych</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
