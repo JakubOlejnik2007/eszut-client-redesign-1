26f7ed1632d33866f50ce14c394c87f5b9be6bd0
Mati <90572281+matiEU07@users.noreply.github.com>
2024-09-13 17:24:18 +0200

    add menu bar buttons

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -7,11 +7,10 @@ export const Navbar = (props:any) => {
 
             <div className='logo'>ESZUT</div>
 
-            {/* props.loggedin ?
-            <div>    
+            {/* props.loggedin ? */}
             <button className='navButton'>Admin</button>
+
             <button className='navButton'>logIn</button>
-            </div> */}
             
         </nav>  
 
