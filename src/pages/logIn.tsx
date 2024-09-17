7f52eac184bfd137d5476a5bd3e399994c115094
Mati <90572281+matiEU07@users.noreply.github.com>
2024-09-17 09:42:34 +0200

    login page improvements

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -3,9 +3,10 @@ export const LogInScreen = () => {
         <>
    
         <div style={{backgroundColor: '', width: '100%'}}className="content-padding text-justify">
-            <h3 style={{textAlign: "center"}}>Zaloguj się z Microsoft</h3>
+            <h3 style={{textAlign: "center"}}>Zaloguj się kontem Microsoft</h3>
+            <center>
             <button className="microsoftLoginButton"><span className="micLogo" />Sign in with microsoft</button>
-            
+            </center>
         </div>        
 
         </>
