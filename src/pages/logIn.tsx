865d02aa3aa30780b1e5eddf21765dc1b9178829
Mati <90572281+matiEU07@users.noreply.github.com>
2024-09-17 09:34:12 +0200

    add basic log in with microsoft button css, add microsoft logo svg, improved vertical padding in some pages

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -1,10 +1,10 @@
-export const LogInScreen = (props:any) => {
+export const LogInScreen = () => {
     return(
         <>
    
         <div style={{backgroundColor: '', width: '100%'}}className="content-padding text-justify">
             <h3 style={{textAlign: "center"}}>Zaloguj się z Microsoft</h3>
-            <button className="microsoftLoginButton">Sign in with microsoft</button>
+            <button className="microsoftLoginButton"><span className="micLogo" />Sign in with microsoft</button>
             
         </div>        
 
