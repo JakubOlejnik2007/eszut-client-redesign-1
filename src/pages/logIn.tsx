bcf177eccc5b884e7da1d2f0f87e1ddd00c4a265
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-09-17 10:25:07 +0200

    Login fix

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -1,12 +1,17 @@
+import { AuthData } from "../auth/AuthWrapper"
+
 export const LogInScreen = () => {
-    return(
+
+    const { login } = AuthData()
+
+    return (
         <>
-   
-        <div style={{backgroundColor: '', width: '100%'}}className="content-padding text-justify">
-            <h3 style={{textAlign: "center"}}>Zaloguj się z Microsoft</h3>
-            <button className="microsoftLoginButton">Sign in with microsoft</button>
-            
-        </div>        
+
+            <div style={{ backgroundColor: '', width: '100%' }} className="content-padding text-justify">
+                <h3 style={{ textAlign: "center" }}>Zaloguj się z Microsoft</h3>
+                <button className="microsoftLoginButton" onClick={login}>Sign in with microsoft</button>
+
+            </div>
 
         </>
     )
