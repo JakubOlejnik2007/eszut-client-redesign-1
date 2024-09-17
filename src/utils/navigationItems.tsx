2e6339c5de06101ed447e07dd38cf33bb2d1e56c
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-09-17 09:21:24 +0200

    Changed some of placeholders in nav list

--- src/utils/navigationItems.tsx
+++ src/utils/navigationItems.tsx
@@ -1,3 +1,5 @@
+import { LogInScreen } from "../pages/logIn";
+import { ReportIssueScreen } from "../pages/reportIssue";
 import TNavigationItem from "../types/navigationItem.type";
 import EUserRole from "../types/userroles.enum";
 import urls from "./urls";
@@ -9,7 +11,7 @@ const NAVIGATION_ITEMS: TNavigationItem[] = [
     id: "1",
     path: urls.client.reportProblem,
     name: "Zgłoś usterkę",
-    element: <Placeholder />,
+    element: <ReportIssueScreen />,
     isMenu: true,
     minUserRole: EUserRole.USER,
   },
@@ -21,7 +23,7 @@ const NAVIGATION_ITEMS: TNavigationItem[] = [
     isMenu: true,
     minUserRole: EUserRole.GUEST,
   },
-  { id: "3", path: urls.client.mainpage, name: "Zaloguj się", element: <Placeholder />, isMenu: false, minUserRole: EUserRole.GUEST },
+  { id: "3", path: urls.client.mainpage, name: "Zaloguj się", element: <LogInScreen />, isMenu: false, minUserRole: EUserRole.GUEST },
   { id: "4.1", path: urls.client.problems, name: "Zgłoszenia", element: <Placeholder />, isMenu: true, minUserRole: EUserRole.ADMIN },
   { id: "4.2", path: urls.client.archive, name: "Archiwum", element: <Placeholder />, isMenu: true, minUserRole: EUserRole.ADMIN },
   { id: "4.5", path: urls.client.displaylog, name: "Dziennik LOG", element: <Placeholder />, isMenu: true, minUserRole: EUserRole.ADMIN }
