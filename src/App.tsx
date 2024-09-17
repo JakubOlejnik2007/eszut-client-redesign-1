d8013daa106626e9fdb342364b02a738b6002163
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-09-17 09:09:19 +0200

    Added router to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@
 import PWABadge from './PWABadge.tsx'
 import Navbar from './components/navbar.tsx'
 import { ReportIssueScreen } from './pages/reportIssue.tsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
-
+import NAVIGATION_ITEMS from "./utils/navigationItems"
 
 function App() {
   return (
@@ -12,11 +13,11 @@ function App() {
         <Navbar />
 
         <div className='window'>
-
-          <div className='titleBar'>Zgłoś usterkę</div>
-          <div className='windowContent'>
-            <ReportIssueScreen></ReportIssueScreen>
-          </div>
+          <Router>
+            <Routes>
+              {NAVIGATION_ITEMS.map(item => <Route path={item.path} element={item.element} />)}
+            </Routes>
+          </Router>
         </div>
       </main>
       <PWABadge />
