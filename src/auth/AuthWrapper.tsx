fad9c47f19ec1b29e58fd2cd8df0903f4e621f6e
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-09-17 10:24:06 +0200

    Added auth wrapper

---
+++ src/auth/AuthWrapper.tsx
@@ -0,0 +1,127 @@
+import { AuthenticationResult, EventType, PublicClientApplication } from "@azure/msal-browser";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom/dist";
+import urls from "../utils/urls";
+import EUserRole from "../types/userroles.enum";
+import axios from "axios";
+import config from "../utils/config";
+
+const msalConfig = {
+    auth: {
+        clientId: 'e4c482a1-9923-4462-bf05-b70d64942c19', // Twój Client ID
+        authority: 'https://login.microsoftonline.com/84867874-5f7d-4b12-b070-d6cea5a3265e', // Twój Tenant ID
+        redirectUri: 'http://localhost:5173',
+    },
+};
+
+interface IUser {
+    role: EUserRole;
+    AuthResult: AuthenticationResult;
+}
+
+const AuthContext = createContext<{
+    user: IUser | null,
+    login: (() => void),
+    logout: () => void
+}>({
+    user: null,
+    login: () => { },
+    logout: () => { }
+});
+export const AuthData = () => useContext(AuthContext);
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+export const AuthWrapper = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<IUser | null>(null);
+    const [isInitialized, setIsInitialized] = useState(false);
+    console.log(user)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const initializeMsal = async () => {
+            await msalInstance.initialize();
+            try {
+                msalInstance.addEventCallback((event) => {
+                    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+                        const authResult = event.payload as AuthenticationResult;
+                        setUser({
+                            AuthResult: authResult,
+                            role: EUserRole.GUEST
+                        });
+                    }
+                });
+
+                const accounts = msalInstance.getAllAccounts();
+                if (accounts.length > 0) {
+                    msalInstance.setActiveAccount(accounts[0]);
+                }
+
+                setIsInitialized(true);
+            } catch (error) {
+                console.error('Błąd podczas inicjalizacji MSAL', error);
+            }
+        };
+
+        initializeMsal();
+    }, []);
+
+    /*useEffect(() => {
+      const userDataFromSession = sessionStorage.getItem("user");
+      if (userDataFromSession) {
+        const parsedUserData = JSON.parse(userDataFromSession);
+        setUser(parsedUserData);
+      }
+    }, []);*/
+
+    const login = async () => {
+        if (!isInitialized) {
+            console.error('MSAL nie jest jeszcze zainicjalizowany');
+            return;
+        }
+
+        try {
+            const loginResponse = await msalInstance.loginPopup({
+                scopes: ["api://e4c482a1-9923-4462-bf05-b70d64942c19/App"], // Uprawnienia potrzebne do pobierania zespołów
+            });
+            setUser({
+                AuthResult: loginResponse,
+                role: EUserRole.GUEST
+            });
+
+            const response = await axios.get(`${config.backend}${urls.backend.auth.getUserRole}`, {
+                headers: {
+                    Authorization: `Bearer ${loginResponse.accessToken}`,
+                },
+            })
+            console.log(response.data);
+            setUser((prevState) => {
+                return {
+                    ...prevState,
+                    role: response.data.role as EUserRole,
+                    AuthResult: prevState?.AuthResult ?? undefined,
+                } as IUser;
+            });
+            navigate(urls.client.reportProblem)
+
+            // Pobranie listy zespołów po zalogowaniu
+            //await fetchTeams(loginResponse.accessToken);
+            //await testToken(loginResponse.accessToken)
+        } catch (error) {
+            console.error('Błąd podczas logowania', error);
+        }
+    };
+
+    const logout = async () => {
+        //sessionStorage.removeItem("user");
+        setUser(null);
+        navigate("/");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
