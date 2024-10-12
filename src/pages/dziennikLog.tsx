c01de4c2ec1a19c70b6c9d669ded46fd9d6bb5ce
Mati <90572281+matiEU07@users.noreply.github.com>
2024-10-12 13:08:58 +0200

    update the dziennik log page

--- src/pages/dziennikLog.tsx
+++ src/pages/dziennikLog.tsx
@@ -6,10 +6,13 @@ export const DziennikLog = () => {
 
 
 
-            <LogElement/>
-            <LogElement/>
-            <LogElement/>
-            <LogElement/>
+
+            <LogElement type="error" logValue="Skill issue" timeStamp="04:40 20.13.2014"/>
+            <LogElement type="error" logValue="Skill issue" timeStamp="04:40 20.13.2014"/>
+            <LogElement type="warning" logValue="Możesz mieć skill issue" timeStamp="04:40 20.13.2014"/>
+            <LogElement type="info" logValue="skibi" timeStamp="04:40 20.13.2014"/>
+            <LogElement type="info" logValue="do toilet lorem ipsum bryk bryknie do bryki" timeStamp="04:40 20.13.2014"/>
+            <LogElement type="warning" logValue="Skill issue" timeStamp="04:40 20.13.2014"/>
 
 
             <br/>
@@ -22,20 +25,22 @@ export const DziennikLog = () => {
 }
 
 
-export const LogElement = () => {
+export const LogElement = (type, logValue, timeStamp) => {
     return(
 
 
 
             <div className="logElement"> 
 
-            <div style={{backgroundColor: '', width: '74%', height: '25px'}}>ERROR: Aziur nie działa</div> 
+            <div className="logError logIcon" style={{marginLeft:'15px', marginRight:'15px'}}></div>
 
-            <div style={{borderColor: '#191919', width: '0.5%', height: '25px', borderRightWidth: '1px', borderRightStyle: 'solid'}}></div>
-            <div style={{borderColor: '', width: '0.5%', height: '25px'}}></div>
+            <div style={{borderColor: '#191919', width: '0%', height: '100%', borderRightWidth: '1px', borderRightStyle: 'solid'}}></div>
 
-            <div style={{backgroundColor: '', width: '25%', height: '25px'}}>10:25 10.25.2025</div>
+            <div style={{backgroundColor: '', width: '80%', height: '100%', paddingLeft: '15px'}}>Aziur nie działa</div> 
 
+            <div style={{borderColor: '#191919', width: '0%', height: '100%', borderRightWidth: '1px', borderRightStyle: 'solid'}}></div>
+
+            <div style={{backgroundColor: '', width: '20%', height: '100%', textAlign: 'right', paddingRight: '15px'}}>10:25 10.25.2025</div>
 
             </div>
         )
