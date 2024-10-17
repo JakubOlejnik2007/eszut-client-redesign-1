1c660d434038c455435c20b1d72d4c6e64d1a7c5
JakubOlejnik2007 <jacobole2000@gmail.com>
2024-10-17 18:23:55 +0200

    Fixed problem with position of modal

--- src/components/ProblemModal.tsx
+++ src/components/ProblemModal.tsx
@@ -1,11 +1,14 @@
+import ReactDOM from "react-dom";
+
 const ProblemModal = () => {
-    return(
+    return ReactDOM.createPortal(
         <div className="modalContainer">
             <div className="modal">
-            this is a full size modal
+                this is a full size modal
             </div>
-        </div>
+        </div>, document.body
     )
 }
-    
+
+
 export default ProblemModal
--- src/components/UnsolvedProblem.tsx
+++ src/components/UnsolvedProblem.tsx
@@ -5,6 +5,9 @@ import { ENotifType } from "../types/notification.interface";
 import { Notif } from "./notificationsWrapper";
 import ProblemModal from "./ProblemModal";
 
+
+import ReactDOM from "react-dom";
+
 const UnsolvedProblem = ({ _id, categoryName, placeName, whoName, whoEmail, what, priority, when, whoDealsName, whoDealsEmail, isUnderRealization }: any, refreshQuery: () => void) => {
 
     const { user } = AuthData();
@@ -50,6 +53,13 @@ const UnsolvedProblem = ({ _id, categoryName, placeName, whoName, whoEmail, what
             displayNotif({ message: "Aktualizacja problemu nie powiodła się", type: ENotifType.ERROR });
         }
     }
+
+
+    const Modal = ({ children }: { children: React.ReactNode }) => {
+        
+    }
+
+
 
     return (
         <>
