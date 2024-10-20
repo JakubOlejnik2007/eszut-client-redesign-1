abfdd13ef1c03d4ca851ea607a919a8507899ec9
Mati <90572281+matiEU07@users.noreply.github.com>
2024-10-20 12:04:57 +0200

    add CloseButton icon

--- src/components/ProblemModal.tsx
+++ src/components/ProblemModal.tsx
@@ -5,6 +5,7 @@ const ProblemModal = () => {
         <div className="modalContainer">
             <div className="modal">
             <h2>problem title</h2>
+            <div className="CloseButton"></div>
             time left: 69min<br/>
             sala: 10<br/>
             reporter<br/>
