0d7feb792f36ea7842a885d6534b275850461d1d
Mati <m.stoch2007@gmail.com>
2024-10-21 22:42:25 +0200

    add stuff

--- src/components/ProblemModal.tsx
+++ src/components/ProblemModal.tsx
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom";
 const ProblemModal = () => {
     return ReactDOM.createPortal(
         <div className="modalContainer">
-            
+
             <div className="modal">
-            <div className="modalTitle">Zgłoszenie: title</div>
-            <div className="modalDescription">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laudantium sunt at corporis similique nisi, placeat pariatur natus recusandae quod praesentium corrupti neque eligendi facere dolorem rerum explicabo velit suscipit.
-            </div>
-            <div className="modalInfo"></div>
-            <div className="closeButton"></div>
+                <div className="modalTitle">Zgłoszenie: title</div>
+                <div className="modalDescription">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laudantium sunt at corporis similique nisi, placeat pariatur natus recusandae quod praesentium corrupti neque eligendi facere dolorem rerum explicabo velit suscipit.
+                </div>
+                <div className="modalInfo">sdfsdfsdfsdfsdf</div>
+                <div className="closeButton"></div>
+                <div className="bottomModalPart"><button className="mainButton">test</button><button  className="mainButton">test</button><button  className="mainButton">Ukończ</button></div>
             </div>
         </div>, document.body
     )
