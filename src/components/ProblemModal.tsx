cff39b6f713cef0b4bf9932ce7d80c4383e77830
Mati <90572281+matiEU07@users.noreply.github.com>
2024-12-19 12:10:19 +0100

    fix shitty code formatting

--- src/components/ProblemModal.tsx
+++ src/components/ProblemModal.tsx
@@ -19,18 +19,19 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
 
                 <div className="modalInfo">
                     <div className="progressBar" >
-                        <DaysToDeadlineSpan priority={priority} reportDate={reportDate} />
-                        <TimeChart procent={timeSpentPercentage(reportDate, priority)} />
+                        <DaysToDeadlineSpan priority={priority} reportDate={reportDate}/>
+                        <TimeChart procent={timeSpentPercentage(reportDate, priority)}/>
                     </div>
 
                     <br></br>
                     Zgłaszający: <WhoReportedLink whoEmail={whoEmail} whoName={whoName} reportDate={reportDate} categoryName={categoryName} placeName={placeName} whoDealsEmail={whoDealsEmail} whoDealsName={whoDealsName} isUnderRealization={isUnderRealization} priority={priority} what={what} />
                     Rozwiązywane przez: Ciebie<br /><br />
-                    Data zgłoszenia: {reportDate.toLocaleDateString("pl")}<br />
+                    Data zgłoszenia: {reportDate.toLocaleDateString("pl")}<br/>
                     Data rozwiązania: nie rozwiązane
                     <br /><br />waga: {priority}
 
                 </div>
+                
                 <div className="closeButton" onClick={handleClose}></div>
                 <div className="bottomModalPart">
                     <button className="mainButton edit">g</button>
