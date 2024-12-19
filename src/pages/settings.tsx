fecdadc6fc9fb9770895a18ce124ce122f742f99
Mati <90572281+matiEU07@users.noreply.github.com>
2024-12-19 13:47:14 +0100

    add settings page

---
+++ src/pages/settings.tsx
@@ -0,0 +1,16 @@
+export const SettingsScreen = () => {
+    return (
+        <>
+            <center>
+                <div style={{ backgroundColor: '', width: '75%' }} className="content-padding text-justify">
+                    <h3 style={{ textAlign: "center" }}>Ustawienia</h3>
+
+                    witaj w ustawieniach. Ta część aplikacji pomoze ci ustawiac ustawienia na tak jask chcesz je suatawic sjdnalsdnksdkf
+
+                </div>
+            </center>
+        </>
+    )
+}
+
+
