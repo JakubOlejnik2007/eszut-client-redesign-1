dc0a666448d7295d9b26ed00d1fcd730df70f87b
Mati <m.stoch2007@gmail.com>
2024-12-23 14:40:13 +0100

    fix switch alignment

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,17 +1,21 @@
 const SettingsScreen = () => {
     return (
         <>
+
                 <div style={{ backgroundColor: '', width: '100%' }} className="content-padding text-justify">
                     <h3 style={{ textAlign: "center" }}>Ustawienia</h3>
 
+                    <div style={{textAlign: "center"}}>
                     wysoki kontrast
-
-                    <label className="switch">
-  <input type="checkbox"></input>
-  <span className="slider"></span>
-</label>
-                    
-
+                    <label className="switch"><input type="checkbox"></input><span className="slider"></span></label>
+                    <br></br>
+                    wysoki kontrast
+                    <label className="switch"><input type="checkbox"></input><span className="slider"></span></label>
+                    <br></br>
+                    wysoki kontrast
+                    <label className="switch"><input type="checkbox"></input><span className="slider"></span></label>
+                    <br></br>
+                    </div>
                 </div>
         </>
     )
