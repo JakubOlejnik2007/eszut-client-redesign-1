bbe06f17d0f0b7d4bd3123dca9a0183e20156f3f
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-01-08 21:15:53 +0100

    Updating data about problems

--- src/components/problems/unsolved-problem/UnsolvedProblemModal.tsx
+++ src/components/problems/unsolved-problem/UnsolvedProblemModal.tsx
@@ -10,11 +10,20 @@ import MapPriorityToWords from "../../partials/MapPriorityToWords";
 import TimeChart from "../../partials/TimeChart";
 import { useState } from "react";
 import { useQuery } from "react-query";
-import { getCategories, getPlaces } from "../../../service/apiFetchFunctions";
+import { getCategories, getPlaces, putUpdateUnsolvedProblem } from "../../../service/apiFetchFunctions";
+import { ENotifType } from "../../../types/notification.interface";
+import NotificationsWrapper, { Notif } from "../../notificationsWrapper";
 
 interface IToggleEditableButtonProps {
     isSingle: boolean;
     toggle: () => void;
+}
+
+interface IEditableData {
+    categoryId: string;
+    placeId: string;
+    priority: string;
+    wasChanged: boolean;
 }
 
 const ToggleEditableButton = ({ isSingle, toggle }: IToggleEditableButtonProps) => {
@@ -28,13 +37,37 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
     const categoriesQuery = useQuery("categories", getCategories, { staleTime: 60000 });
     const placesQuery = useQuery("places", getPlaces, { staleTime: 60000 });
 
+    console.log(categoriesQuery.data, placesQuery.data)
+
     const [isEdit, setIsEdit] = useState<boolean>(false);
+    const [editableData, setEditableData] = useState<IEditableData>({ categoryId: categoryId, placeId: placeId, priority: priority.toString(), wasChanged: false });
+
     const { user } = AuthData();
+    const { displayNotif } = Notif();
+
     const USER_EMAIL = user?.AuthRole.account.username;
 
     const reportDate = new Date(when);
 
     console.log(timeSpentPercentage(reportDate, priority), _id)
+
+
+    const toggleEditable = () => {
+        if (isEdit)
+            try {
+                putUpdateUnsolvedProblem(user?.AuthRole.accessToken as string, editableData.priority, editableData.placeId, editableData.categoryId, _id);
+                setIsEdit(!isEdit);
+                if (editableData.wasChanged) displayNotif({ message: "Problem został zaktualizowany", type: ENotifType.SUCCESS });
+            } catch (e) {
+                console.log(e)
+                displayNotif({ message: "Wystąpił błąd", type: ENotifType.ERROR });
+            }
+        else setIsEdit(!isEdit);
+    }
+
+    if (placesQuery.isLoading || categoriesQuery.isLoading) {
+        return <img src="src/assets/loading.gif" className="spinner"></img>
+    }
 
     return ReactDOM.createPortal(
         <div className="modalContainer fade-in" onClick={handleClose}>
@@ -46,7 +79,7 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
                 <div className="modalInfo">
                     <div className="progressBar" >
                         <DaysToDeadlineSpan priority={priority} reportDate={reportDate} />
-                        <TimeChart procent={timeSpentPercentage(reportDate, priority)} />
+                        <TimeChart procent={timeSpentPercentage(reportDate, parseInt(editableData.priority))} />
                     </div>
 
                     <br></br>
@@ -60,15 +93,15 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
                     {
                         isEdit ?
                             <>
-                                Kategoria: <select value={categoryId}>
+                                Kategoria: <select value={editableData.categoryId} onChange={(e) => setEditableData({ ...editableData, categoryId: e.target.value, wasChanged: true })}>
                                     {categoriesQuery.data?.map((category: any) => <option key={category._id} value={category._id}>{category.name}</option>)}
                                 </select>
 
-                                Miejsce: <select value={placeId}>
+                                Miejsce: <select value={editableData.placeId} onChange={(e) => setEditableData({ ...editableData, placeId: e.target.value, wasChanged: true })}>
                                     {placesQuery.data?.map((place: any) => <option key={place._id} value={place._id}>{place.name}</option>)}
                                 </select>
 
-                                Priorytet: <select value={priority}>
+                                Priorytet: <select value={editableData.priority} onChange={(e) => setEditableData({ ...editableData, priority: e.target.value, wasChanged: true })}>
                                     <option value="1">Wysoki</option>
                                     <option value="2">Średni</option>
                                     <option value="3">Niski</option>
@@ -76,9 +109,9 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
                             </>
                             :
                             <>
-                                Kategoria: {categoryName}<br />
-                                Miejsce: {placeName}<br />
-                                Priorytet: <MapPriorityToWords priority={priority} /> <br />
+                                Kategoria: {categoriesQuery.data.find((item: any) => item._id == editableData.categoryId).name}<br />
+                                Miejsce: {placesQuery.data.find((item: any) => item._id == editableData.placeId).name}<br />
+                                Priorytet: <MapPriorityToWords priority={parseInt(editableData.priority)} /> <br />
                             </>
                     }
 
@@ -88,7 +121,7 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
                 <div className="closeButton" onClick={handleClose}></div>
                 <div className="bottomModalPart">
                     {
-                        USER_EMAIL === whoDealsEmail || !isUnderRealization ? <ToggleEditableButton isSingle={!isUnderRealization} toggle={() => setIsEdit(!isEdit)} /> : null
+                        USER_EMAIL === whoDealsEmail || !isUnderRealization ? <ToggleEditableButton isSingle={!isUnderRealization} toggle={toggleEditable} /> : null
                     }
                     {
                         USER_EMAIL === whoDealsEmail ? <><button className="mainButton marginLeft" onClick={handleReject}>Zrezygnuj</button>
@@ -101,4 +134,4 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
 }
 
 
-export default ProblemModal
+export default ProblemModal;
