5f781af1955b196ba48c5fcede90445e3b39ba11
JakubOlejnik2007 <jacobole2000@gmail.com>
2025-01-08 09:46:02 +0100

    Displaying additional info about unsolved problems and switching to editable mode

--- src/components/problems/unsolved-problem/UnsolvedProblemModal.tsx
+++ src/components/problems/unsolved-problem/UnsolvedProblemModal.tsx
@@ -8,9 +8,27 @@ import WhoReportedLink from "../../mail/WhoReportedLink";
 import DaysToDeadlineSpan from "../../partials/DaysToDeadlineSpan";
 import MapPriorityToWords from "../../partials/MapPriorityToWords";
 import TimeChart from "../../partials/TimeChart";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { getCategories, getPlaces } from "../../../service/apiFetchFunctions";
 
-const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoName, whoEmail, what, when, priority, categoryName, placeName, whoDealsEmail, whoDealsName, isUnderRealization }: IUnsolvedProblemModal) => {
+interface IToggleEditableButtonProps {
+    isSingle: boolean;
+    toggle: () => void;
+}
 
+const ToggleEditableButton = ({ isSingle, toggle }: IToggleEditableButtonProps) => {
+    return (
+        <button className={`mainButton edit ${isSingle ? "singlebuttonoffset" : ""}`} onClick={toggle}>g</button>
+    )
+}
+
+const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoName, whoEmail, what, when, priority, categoryName, placeName, whoDealsEmail, whoDealsName, isUnderRealization, placeId, categoryId }: IUnsolvedProblemModal) => {
+
+    const categoriesQuery = useQuery("categories", getCategories, { staleTime: 60000 });
+    const placesQuery = useQuery("places", getPlaces, { staleTime: 60000 });
+
+    const [isEdit, setIsEdit] = useState<boolean>(false);
     const { user } = AuthData();
     const USER_EMAIL = user?.AuthRole.account.username;
 
@@ -34,18 +52,47 @@ const ProblemModal = ({ handleClose, handleReject, handleMarkAsSolved, _id, whoN
                     <br></br>
                     Zgłaszający: <WhoReportedLink whoEmail={whoEmail} whoName={whoName} reportDate={reportDate} categoryName={categoryName} placeName={placeName} whoDealsEmail={whoDealsEmail} whoDealsName={whoDealsName} isUnderRealization={isUnderRealization} priority={priority} what={what} />
                     Rozwiązywane przez: {whoDealsEmail === USER_EMAIL ? "Ciebie" : <WhoDealsLink whoEmail={whoEmail} whoName={whoName} reportDate={reportDate} categoryName={categoryName} placeName={placeName} whoDealsEmail={whoDealsEmail} whoDealsName={whoDealsName} isUnderRealization={isUnderRealization} priority={priority} what={what} />}<br /><br />
+                    <br />
                     Zgłoszenie: {reportDate.toLocaleString("pl")}<br />
                     Termin: {(getDeadlineDate(reportDate, priority)).toLocaleString("pl")}<br />
-                    Rozwiązanie: nie rozwiązane
-                    <br /><br />Priorytet: <MapPriorityToWords priority={priority} />
+
+                    <br /><br />
+                    {
+                        isEdit ?
+                            <>
+                                Kategoria: <select value={categoryId}>
+                                    {categoriesQuery.data?.map((category: any) => <option key={category._id} value={category._id}>{category.name}</option>)}
+                                </select>
+
+                                Miejsce: <select value={placeId}>
+                                    {placesQuery.data?.map((place: any) => <option key={place._id} value={place._id}>{place.name}</option>)}
+                                </select>
+
+                                Priorytet: <select value={priority}>
+                                    <option value="1">Wysoki</option>
+                                    <option value="2">Średni</option>
+                                    <option value="3">Niski</option>
+                                </select>
+                            </>
+                            :
+                            <>
+                                Kategoria: {categoryName}<br />
+                                Miejsce: {placeName}<br />
+                                Priorytet: <MapPriorityToWords priority={priority} /> <br />
+                            </>
+                    }
+
 
                 </div>
 
                 <div className="closeButton" onClick={handleClose}></div>
                 <div className="bottomModalPart">
-                    <button className={`mainButton edit ${USER_EMAIL != whoDealsEmail ? "singlebuttonoffset" : ""}`}>g</button>
                     {
-                        USER_EMAIL === whoDealsEmail ? <><button className="mainButton marginLeft" onClick={handleReject}>Zrezygnuj</button><button className="mainButton successButton marginLeft" onClick={handleMarkAsSolved}>Ukończ</button></> : null
+                        USER_EMAIL === whoDealsEmail || !isUnderRealization ? <ToggleEditableButton isSingle={!isUnderRealization} toggle={() => setIsEdit(!isEdit)} /> : null
+                    }
+                    {
+                        USER_EMAIL === whoDealsEmail ? <><button className="mainButton marginLeft" onClick={handleReject}>Zrezygnuj</button>
+                            <button className="mainButton successButton marginLeft" onClick={handleMarkAsSolved}>Ukończ</button></> : null
                     }
                 </div>
             </div>
