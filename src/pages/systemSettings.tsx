ae44150dad62f5ecfe09f84e022d7de92870d564
Mati <90572281+matiEU07@users.noreply.github.com>
2025-01-28 17:12:20 +0100

    add interactive tab button, success styling, fix padding

--- src/pages/systemSettings.tsx
+++ src/pages/systemSettings.tsx
@@ -10,7 +10,9 @@ const SystemSettings = () => {
                         <TabElement name={"problem z komputerem"} importance={2}></TabElement>
                         <TabElement name={"problem z komputerem"} importance={2}></TabElement>
                         <TabElement name={"problem z komputerem"} importance={2}></TabElement>
-                        <div className="intTabElement"><input type="text" className="intTabInput" placeholder="dodaj nową kategorię..."></input></div>
+                        {/* adding a new category */}
+                        <div className="intTabElement"><input type="text" className="intTabInput" placeholder="dodaj nową kategorię..."></input>
+                        <button className="intTabButton intSuccess">Dodaj</button></div>
                     </div>
 
 
@@ -32,9 +34,7 @@ export const TabElement = ({ name, importance }: TabElementProps) => {
     return (
 
         <div className="intTabElement">
-
-            {name}
-
+            {name}<button className="intTabButton">usuń</button>
         </div>
     )
 }
