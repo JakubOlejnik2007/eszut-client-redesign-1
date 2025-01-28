2e1500eb88b9f7a737b6126501180b6fbddbf1b7
Mati <90572281+matiEU07@users.noreply.github.com>
2025-01-28 11:20:16 +0100

    add interactiveTabElements and interactiveTab

--- src/pages/systemSettings.tsx
+++ src/pages/systemSettings.tsx
@@ -3,9 +3,16 @@ const SystemSettings = () => {
         <>
                 <div style={{ backgroundColor: '', width: '100%', margin: '-15px'  }} className="content-padding text-justify">
                     <h3 style={{ textAlign: "center" }}>Zarządzaj kategoriami</h3>
+                    <div className="intTabContainer">
+                        <TabElement name={"problem z komputerem"} importance={2}></TabElement>
+                        <TabElement name={"problem z komputerem"} importance={2}></TabElement>
+                        <TabElement name={"problem z komputerem"} importance={2}></TabElement>
+                        <TabElement name={"problem z komputerem"} importance={2}></TabElement>
+                        <TabElement name={"problem z komputerem"} importance={2}></TabElement>
+                        <TabElement name={"problem z komputerem"} importance={2}></TabElement>
+                        <div className="intTabElement"><input type="text" className="intTabInput" placeholder="dodaj nową kategorię..."></input></div>
+                    </div>
 
-                        <TabElement expiryDate={2} token="t" userEmail="g" type={3}></TabElement>
-                    
 
                 </div>
         </>
@@ -16,28 +23,17 @@ export default SystemSettings;
 
 
 interface TabElementProps {
-    expiryDate: number;
-    token: string;
-    userEmail: string;
-    type: number;
+    name: string;
+    importance: number;
 }
 
-export const TabElement = ({ expiryDate, token, userEmail, type }: TabElementProps) => {
-
-    const parsedDate = new Date(expiryDate);
+export const TabElement = ({ name, importance }: TabElementProps) => {
 
     return (
 
+        <div className="intTabElement">
 
-
-        <div className="logElement" style={{fontFamily: "sfMono", fontSize: "0.9rem"}}>
-
-            <div data-tooltip={userEmail} style={{ backgroundColor: '', width: '68%', height: '25px', transform: 'translateY(2.5px)', textAlign: 'left', marginLeft: '2%', borderColor: '--var()' }}>{token}</div>
-
-            <div style={{borderColor: 'var(--tableAccent)', width: '0.5%', height: '25px', borderRightWidth: '1px', borderRightStyle: 'solid' }}></div>
-            <div style={{borderColor: '', width: '0.5%', height: '25px' }}></div>
-            <div style={{backgroundColor: '', width: '50%', height: '25px', transform: 'translateY(2.5px)', textAlign: 'right', marginRight: '2%'}} className="secondary">{parsedDate.toLocaleString()}</div>
-
+            {name}
 
         </div>
     )
