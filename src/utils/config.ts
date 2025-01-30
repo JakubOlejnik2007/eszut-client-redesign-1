3f87c8ca711158185ddde853e3ef0908f38c4e64
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-01-31 00:24:41 +0100

    Fixed problem with access to the api on prod

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    backend: "http://localhost:5100" //"https://eszut-api.tenco.waw.pl"
+    backend: /*"http://localhost:5100"*/ "https://eszut-api.tenco.waw.pl"
 }
 
 export const msalConfig = {
